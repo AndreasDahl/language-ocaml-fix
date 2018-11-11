4421e38f374b8083b849792a05dd5c6da0c64959
Pieter Goetschalckx <3.14.e.ter@gmail.com>
2018-11-11 01:13:18 +0100

    Use correct scope name for injection

--- main.js
+++ main.js
@@ -1,40 +1,29 @@
 exports.activate = function() {
   if (!atom.grammars.addInjectionPoint) return
 
-  atom.grammars.addInjectionPoint('ocamllex', {
+  atom.grammars.addInjectionPoint('source.ocamllex', {
     type: 'lexer_definition',
 
     language: (node) => 'ocaml',
 
     content: (node) => node.children
       .filter((child) => child.type == 'action')
-      .map((node) => node.children.find((child) => child.type == 'ocaml'))
+      .map((node) => node.children[1])
   })
 
-  atom.grammars.addInjectionPoint('ocamllex', {
+  atom.grammars.addInjectionPoint('source.ocamllex', {
     type: 'refill_handler',
 
     language: (node) => 'ocaml',
 
-    content: (node) => node.children.find((child) => child.type == 'ocaml')
+    content: (node) => node.children[2]
   })
 
-  atom.grammars.addInjectionPoint('ocamllex', {
+  atom.grammars.addInjectionPoint('source.ocamllex', {
     type: 'lexer_case',
 
     language: (node) => 'ocaml',
 
-    content: (node) => {
-      const action = node.children.find((child) => child.type == 'action')
-      return action.children.find((child) => child.type == 'ocaml')
-    }
+    content: (node) => node.lastChild.children[1]
   })
-
-  // atom.grammars.addInjectionPoint('ocamllex', {
-  //   type: 'ocaml',
-  //
-  //   language: (node) => 'ocaml',
-  //
-  //   content: (node) => node
-  // })
 }
